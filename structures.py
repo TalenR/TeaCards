838c15ed9b1d42ec4330d543472b3f038d91fe63
Talen Rabe <talen.rabe1@gmail.com>
2018-12-05 16:16:07 -0800

    filled in a bunch of the unit functions

--- structures.py
+++ structures.py
@@ -16,26 +16,29 @@ PositionedCard = namedtuple(
 	"PositionedCard",
 	['card', 'north', 'south', 'east', 'west'])
 
-pcard = PositionedCard("card", "n", "s", "e", "w")
 Supplier = namedtuple("Supplier", ['location', 'size'])
 
 def change_neighbor(pcard, **kwargs):
-	vals = pcard.__attrs__.copy()
+	vals = getattr(pcard).copy()
 	vals.update(kwargs)
 	return PositionedCard(**vals)
+
+def upgrade_supplier(supplier, kwargs):
+	vals = getattr(supplier).copy()
+	vals.update(kwargs)
+	return Supplier(vals)
 
 class Hand(Object):
 
 	def __init__(self, cards):
 		self._cards = cards
 
-	def draw_cards(self, supply):
-		#self._cards.extend(cards)
-		the_cards = supply.draw_cards()
-		self.add_cards(the_cards)
 
-	def add_cards(the_cards)
-		self._cards = self._cards + the_cards
+	def cards_in(the_cards)
+		self._cards += the_cards
+
+	def cards_out(self, number_of_cards)
+		self._cards -= number_of_cards
 
 
 class Supply(Object):
@@ -43,35 +46,49 @@ class Supply(Object):
 	def __init__(self, cards):
 		self._cards = cards
 
-	def draw_cards(self, hand, number_of_cards)
-		self._cards = self.cards - number_of_cards
+	def cards_out(self, number_of_cards):
+		self._cards -= number_of_cards
 
+	def cards_in(self, cards):
+		self._cards += cards 
+		
 
 
 class Catalog(Object):
 
 	def __init__(self, positioned_cards):
-		self._cards = 
+		self._cards = positioned_cards
+
+	def swap(self, cards):
+		change_neighbor(pcard, card = card)
+
+	def get_line(self, location):
+		pass
+		
 
 
 class Company(Object):
 
-	def __init__(self, hand, supplier):
+	def __init__(self, hand, supplier = 1):
 		self._hand = hand
 		self._supplier = supplier
 
 	def take_turn(self):
-		pass
+		TeaCards.take_turn()
 
-	def operate(self, catalog):
-		pass
+	def supply(self, supply, cards):
+		supply.cards_in(cards)
+		self._hand.cards_out()
+		self.supplier = upgrade_supplier(self.supplier) 
 
-	def trade(self, catalog, supply):
-		pass
+	def trade(self, catalog, supply, cards):
+		catalog.swap()
+		self._hand.cards_out()
 
-	def supply(self, supply):
-		the_cards = supply.draw_cards(4)
-		self._hand.cards_drawn(the_cards)
+	def operate(self, supply, catalog):
+		size_line = catalog.get_line(self.supplier.location)
+		the_cards = supply.cards_out()
+		self._hand.cards_in(the_cards)
 
 
 class TeaCards(Object):
@@ -95,4 +112,15 @@ class TeaCards(Object):
 			self._companies.pop(turn_taker)
 			self._turn_takers = repeat(self._companies)
 
-			sdCc
+	def initiate_game(self, deck_file, companies,catalog_shape):
+		def get_deck(deck_file):
+			pass
+
+		self.supply = get_deck(deck_file)
+
+		def build_catalog(decK):
+			pass
+
+		self.catalog = build_catalog(self.supply)
+
+
