329c3b383aa95221a612aff29696fb29b2449495
Talen Rabe <talen.rabe1@gmail.com>
2018-12-10 19:55:37 -0800

    continue on structures and tests

--- structures.py
+++ structures.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import itertools
-from copy import copy
+from copy import copy, deepcopy
+from itertools import cycle, islice
 
 RED = "red"
 ORANGE = "orange"
@@ -14,7 +15,8 @@ NUMBERS = range(1, 6)
 
 
 def change_pcard(pcard, **kwargs):
-	return pcard._replace(**kwargs)
+	new_pcard = pcard._replace(**kwargs)
+	return new_pcard
 	
 def is_in(card, cards):
 	for c in cards:
@@ -51,7 +53,7 @@ class Hand():
 		parameters:
 			cards: a list of card objects
 		'''
-		self._cards = self._cards + cards
+		return Hand(cards = self._cards + cards)
 
 	def cards_out(self, cards):
 		'''
@@ -62,19 +64,19 @@ class Hand():
 		cards_copy = copy(self._cards)
 		for card in cards:
 			cards_copy.remove(card)
-		self._cards = cards_copy
+		return Hand(cards = cards_copy)
 
 class Supplier():
 
 	def __init__(self, location, size = 1):
-		self.size = size
-		self.location = location
+		self._size = size
+		self._location = location
 
 	def upgrade(self):
-		self.size += 1
+		return Supplier(size = self._size + 1, location = self._location)
 
 	def downgrade(self):
-		self.size -= 1
+		return Supplier(size = self._size - 1, location = self._location)
 
 
 class Supply():
@@ -82,13 +84,15 @@ class Supply():
 	def __init__(self, cards =[]):
 		self._cards = cards
 
-	def cards_out(self, out_cards):
+	def draw(self, out_cards):
 		the_cards = self._cards[:out_cards]
-		del self._cards[:out_cards]
 		return the_cards
 
+	def cards_out(self, out_cards):
+		return Supply(cards = self._cards[out_cards:])
+
 	def cards_in(self, cards):
-		self._cards = self._cards + cards
+		return Supply(cards = self._cards + cards)
 		
 
 
@@ -111,24 +115,20 @@ class Catalog():
 		""" updates p-card of old_card with card = card, and updates its neighbors to match. Then writes these to pcards and removes the old_pcard and neighbors
 		"""
 		pcard = self.find_pcard(old_card)
-		old_neighbors = [self.find_pcard(card for card in self.get_neighbors(pcard))]
-
+		old_neighbors = [self.find_pcard(card) for card in self.get_neighbors(pcard)]
+		
 		new_pcard = change_pcard(pcard, card = new_card)
 		new_neighbor_south = change_pcard(self.find_pcard(pcard.south), north = new_card)
 		new_neighbor_north = change_pcard(self.find_pcard(pcard.north), south = new_card)
 		new_neighbor_west = change_pcard(self.find_pcard(pcard.west), east = new_card)
 		new_neighbor_east = change_pcard(self.find_pcard(pcard.east), west = new_card)
 		new_neighbors = [new_neighbor_east, new_neighbor_west, new_neighbor_north, new_neighbor_south]
+		new_cards = self._cards + new_neighbors
+		new_cards = [card for card in new_cards if card.card != default_card and card not in old_neighbors]
 
-		self._cards = self._cards + new_neighbors
-
-		for index in range(len(old_neighbors)):
-			old = old_neighbors[index]
-			if old in self._cards:
-				self._cards.remove(old)
-
-		self._cards.remove(pcard)
-		self._cards = self._cards + [new_pcard]
+		new_cards.remove(pcard)
+		new_cards = new_cards + [new_pcard]
+		return Catalog(positioned_cards = new_cards)
 
 
 	def get_neighbors(self, pcard):
@@ -150,7 +150,7 @@ class Catalog():
 	def get_line(self, company):
 		""" returns combined line_or_color for each color on company's home-card"""
 		pcards = self._cards
-		home_card = company._supplier.location
+		home_card = company._supplier._location
 		colors = home_card._colors
 		return set(flatten([self.line_of_color(home_card, color, pcards) for color in colors]))
 
@@ -164,18 +164,14 @@ class Company():
 	"""
 
 	def __init__(self, supplier, name = '', hand = []):
-		self.name = name
+		self._name = name
 		self._hand = hand
 		self._supplier = supplier
-
-	def take_turn(self, supply, catalog):
-
-		TeaCards.take_turn()
 
 	def supply(self, supply):
 
 		hand = self._hand
-		size = self._supplier.size
+		size = self._supplier._size
 		supply.cards_in(hand)
 		self._hand = []
 		self._supplier.upgrade()
@@ -191,15 +187,17 @@ class Company():
 	def draw(self, supply, number_of_cards):
 		the_cards = supply.cards_out(number_of_cards)
 		self._hand.cards_in(the_cards)
-
-	def operate(self, supply, catalog):
+'''
+	def operate(self, company, supply, catalog):
 		""" increases a company's hand size by either the company's line_size or supplier, whichever is smallest """
-		supplier = self._supplier
+		supplier = company._supplier._size
 		line_size = len(catalog.get_line(self))
-		difference = line_size - supplier.size
+		difference = line_size - supplier._size
+		hand = company._hand
 
 		if difference == 1 or difference == 0:
-			self.draw(supply, supplier.size)
+			new_supply = supply.cards_out(line_size)
+			new_hand = hand.in
 		elif difference >= 2:
 			company.supplier -= 1
 			self.draw((supplier - 2)),
@@ -207,7 +205,7 @@ class Company():
 			self.draw(line_size)
 		elif difference <= -2:
 			company.supplier -= 1
-			self.draw(line)
+			self.draw(line)'''
 
 class TeaCards():
 
@@ -215,7 +213,7 @@ class TeaCards():
 		self._catalog = catalog
 		self._companies = companies
 		self._supply = supply
-		self._turn_takers = repeat(companies)
+		self._turn_takers = cycle(companies)
 
 	def take_turn(self):
 		turn_taker = next(self._turn_takers)
@@ -226,15 +224,79 @@ class TeaCards():
 		if turn_taker.loses_to(self._companies):
 			self.eliminate_company(turn_taker)
 
-	def eliminate_company(self, company):
+	def eliminate_company(self):
 			self._companies.pop(turn_taker)
 			self._turn_takers = repeat(self._companies)
+
+	def get_company(self, company):
+		return self._companies[self._companies.index(company)]
+
+	def replace_company(self, company, new_company):
+		self._companies[self._companies.index(company)] = new_company
+
+
+
+
+	def supply(self, company):
+		new_supply = self._supply.cards_in(company._hand)
+		new_supplier = company._supplier.upgrade()
+
+		new_company = deepcopy(company)
+		new_company._supplier = new_supplier
+		new_company._hand = []
+		self._supply = new_supply
+		self.replace_company(company, new_company)
+
+	def trade(self, company, catalog, supply, old_card, new_card, payment_cards):
+		cost = max(old_card._number, new_card._number)
+		new_hand = company._hand.cards_out(payment_cards + [new_card])
+		new_supply = supply.cards_in(payment_cards + [old_card])
+		new_catalog = catalog.swap(old_card, new_card)
+
+		new_company = deepcopy(company)
+		new_company._hand = new_hand
+
+		self.replace_company(company, new_company)
+		self._supply = new_supply
+		self._catalog = new_catalog
+
+	def operate(self, company, supply, catalog):
+		""" increases a company's hand size by either the company's line_size or supplier, whichever is smallest """
+		supplier = company._supplier
+		size = supplier._size
+		line_size = len(catalog.get_line(company))
+		difference = line_size - supplier._size
+		hand = company._hand
+
+		if difference == 1 or difference == 0:
+			hand = hand.cards_in(supply.draw(size))
+			supply = supply.cards_out(size)
+		elif difference >= 2:
+			print('over operation')
+			supplier = supplier.downgrade()
+			hand = hand.cards_in(supply.draw(size - 2))
+			supply = supply.cards_out(size - 2)
+		elif difference == -1:
+			print('just under operate')
+			hand = hand.cards_in(supply.draw(line_size))
+			supply = supply.cards_out(line_size)
+		elif difference <= -2:
+			print('underoperate')
+			supplier = supplier.downgrade()
+			hand = hand.cards_in(supply.draw(line_size))
+			supply = supply.cards_out(line_size)
+
+		new_company = deepcopy(company)
+		new_company._hand = hand
+		new_company._supplier = supplier
+		self.replace_company(company, new_company)
+		self._supply = supply
 
 	def initiate_game(self, deck_file, companies,catalog_shape):
 
 		def get_deck(deck_file):
 			with open(deck_file) as f:
-				r = np.genfromtxt(f,dtype=str, delimiter = ',')
+				r = np.genfromtxt(f, dtype=str, delimiter = ',')
 				deck = []
 				for i in range(len(r)):
 					colors = set(r[i][0].split(' '))
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import structures
-from structures import Card, PositionedCard, Hand, Supplier, Supply, Catalog, default_card, Company, flatten
+from structures import Card, PositionedCard, Hand, Supplier, Supply, Catalog, default_card, Company, flatten, TeaCards
 from structures import RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET
 from collections import namedtuple
 from copy import copy
@@ -13,25 +13,30 @@ class Test_Pure_Functions(unittest.TestCase):
 
 	def test_change_pcard(self):
 		pcard = pcards[0]
-		card = cards[0]
+		card = cards[1]
 		new_pcard = structures.change_pcard(pcard, card = card)
 		self.assertEqual(pcard.north, new_pcard.north)
 		self.assertEqual(pcard.south, new_pcard.south)
 		self.assertEqual(pcard.east, new_pcard.east)
 		self.assertEqual(pcard.west, new_pcard.west)
 		self.assertEqual(new_pcard.card, card)
+		new_pcard = structures.change_pcard(pcard, south = card)
+		self.assertEqual(new_pcard.north, pcard.north)
+		self.assertEqual(new_pcard.south, card)
+
 
 	def test_is_in(self):
 		card = Card({'red','green'}, 4)
 		self.assertFalse(structures.is_in(card, cards))
-		cards.append(card)
-		self.assertTrue(structures.is_in(card, cards))
+		new_cards = cards + [card]
+		self.assertTrue(structures.is_in(card, new_cards))
 
 	def test_flatten(self):
 		lists = [[1, 2, 3]]
 		self.assertEqual(flatten(lists), [1, 2, 3])
 		lists = [[1, 2, 3], [4]]
 		self.assertEqual(flatten(lists), [1, 2, 3, 4])
+
 		
 #	Tests for Hand methods
 
@@ -39,15 +44,15 @@ class Test_Hands_cards_in(unittest.TestCase):
 
 	def test_cards_in_by_one(self):
 		hand = Hand()
-		hand.cards_in([cards[0]])
-		self.assertEqual(hand._cards, [cards[0]])
+		new_hand = hand.cards_in([cards[0]])
+		self.assertEqual(new_hand._cards, [cards[0]])
 
 class Test_Hands_cards_out(unittest.TestCase):
 
 	def test_cards_out_by_one(self):
 		hand = Hand(cards = [cards[0]])
-		hand.cards_out([cards[0]])
-		self.assertEqual(hand._cards,[])
+		new_hand = hand.cards_out([cards[0]])
+		self.assertEqual(new_hand._cards,[])
 
 #	Tests Supplier methods
 
@@ -55,32 +60,63 @@ class Test_Supplier_upgrade(unittest.TestCase):
 
 	def test_ugrade_by_one(self):
 		supplier = Supplier(size = 1, location = cards[0])
-		supplier.upgrade()
-		self.assertEqual(supplier.size, 2)
+		new_supplier = supplier.upgrade()
+		self.assertEqual(new_supplier._size, 2)
 
 class Test_Supplier_downgrade(unittest.TestCase):
 
 	def test_downgrade_by_one(self):
 		supplier = Supplier(size = 2, location = cards[0])
-		supplier.downgrade()
-		self.assertEqual(supplier.size, 1)
+		new_supplier = supplier.downgrade()
+		self.assertEqual(new_supplier._size, 1)
+
+class Test_Supply_draw(unittest.TestCase):
+
+	def test_draw(self):
+		supply = Supply(cards = cards)
+		cards_drawn = supply.draw(2)
+		self.assertEqual(cards_drawn, cards[:2])
 
 
 class Test_Supply_cards_in(unittest.TestCase):
 
 	def test_cards_in_by_two(self):
 		supply = Supply(cards = [cards[0]])
-		supply.cards_in([cards[1],cards[2]])
-		self.assertEqual(supply._cards, [cards[0], cards[1],cards[2]])
+		new_supply = supply.cards_in([cards[1],cards[2]])
+		self.assertEqual(new_supply._cards, [cards[0], cards[1],cards[2]])
 
 class Test_Supply_cards_out(unittest.TestCase):
 
 	def test_cards_out_by_two(self):
 		supply =Supply(cards = [cards[0], cards[1],cards[2]])
-		supply.cards_out(2)
-		self.assertEqual(supply._cards, [cards[2]])
+		new_supply = supply.cards_out(2)
+		self.assertEqual(new_supply._cards, [cards[2]])
 
 #	Tests Catalog methods
+
+class Test_Catalog_swap(unittest.TestCase):
+
+	def test_swap(self):
+
+		catalog = Catalog(positioned_cards = pcards)
+		old_pcard = catalog.find_pcard(cards[0])
+		
+		new_card = Card(colors = {VIOLET, RED}, number = 1)
+		new_catalog = catalog.swap(cards[0], new_card)
+		self.assertEqual(len(catalog._cards), len(new_catalog._cards))
+		new_pcard = new_catalog.find_pcard(new_card)
+		self.assertEqual(new_pcard.north, old_pcard.north)
+		self.assertEqual(new_pcard.south, old_pcard.south)
+		self.assertEqual(new_pcard.east, old_pcard.east)
+		self.assertEqual(new_pcard.west, old_pcard.west)
+
+		north_neighbor = new_catalog.find_pcard(new_pcard.north)
+		east_neighbor = new_catalog.find_pcard(new_pcard.east)
+
+		self.assertEqual(north_neighbor.south, new_card)
+		self.assertEqual(east_neighbor.west, new_card)
+
+
 
 class Test_Catalog_find_pcard(unittest.TestCase):
 
@@ -122,14 +158,14 @@ class Test_Catalog_get_line(unittest.TestCase):
 		self.assertEqual(catalog.get_line(company), {cards[0], cards[1], cards[2]})
 
 #	Tests for Company methods
-
+'''
 class Test_Company_supply(unittest.TestCase):
 
 	def test_supply_one_to_two(self):
 		company = Company(supplier = Supplier(size = 1, location = cards[0]), hand = cards)
 		supply = Supply(cards = [])
 		company.supply(supply)
-		self.assertEqual(company._supplier.size, 2)
+		self.assertEqual(company._supplier._size, 2)
 		self.assertEqual(company._hand, [])
 		self.assertEqual(supply._cards, cards)
 
@@ -155,7 +191,8 @@ class Test_Company_trade(unittest.TestCase):
 		self.assertEqual(new_supply, supply._cards)
 		self.assertEqual(company._hand._cards, [cards[2]])
 		self.assertEqual(new_neighbors, old_neighbors)
-
+'''
+'''
 class Test_Company_draw(unittest.TestCase):
 
 	def test_draw(self):
@@ -174,15 +211,100 @@ class Test_Company_operate(unittest.TestCase):
 		company.operate(supply, catalog)
 
 		self.assertEqual(len(company._hand._cards), 2)
+'''
+#	Test methods on TeaCards
+
+class Test_TeaCards_get_company(unittest.TestCase):
+
+	def test_get_company(self):
+		company = Company(supplier = Supplier(size = 1, location = cards[0]), hand = cards)
+		tea_cards = TeaCards(catalog = [], supply = [], companies = [company])
+		self.assertEqual(tea_cards.get_company(company), company)
+
+class Test_TeaCards_replace_company(unittest.TestCase):
+
+	def test_replace_company(self):
+		company = Company(supplier = Supplier(size = 1, location = cards[0]), hand = cards)
+		tea_cards = TeaCards(catalog = [], supply = [], companies = [company])
+		new_company = Company(supplier = Supplier(size = 3, location = cards[0]), hand = cards)
+		tea_cards.replace_company(company, new_company)
+		self.assertEqual(tea_cards._companies[0], new_company)
 
 
+class Test_TeaCards_supply(unittest.TestCase):
 
+	def test_supply_one_to_two(self):
+		company = Company(supplier = Supplier(size = 1, location = cards[0]), hand = cards, name = 'comp')
+		supply = Supply(cards = [])
+		tea_cards = TeaCards(catalog = [], supply = supply, companies = [company])
 
+		new_company = Company(supplier = Supplier(size = 2, location = company._supplier._location))
 
+		tea_cards.supply(company)
+		self.assertEqual(tea_cards._companies[0]._supplier._size, 2)
+		self.assertEqual(tea_cards._companies[0]._hand, [])
+		self.assertEqual(tea_cards._supply._cards, cards)
 
+class Test_TeaCards_trade(unittest.TestCase):
 
+	def test_trade(self):
 
+		catalog = Catalog(positioned_cards = pcards)
+		supply = Supply(cards = [])
+		old_card = cards[0]
+		new_card = Card(colors = {BLUE, VIOLET}, number = 2)
+		new_cards = flatten([cards, [new_card]])
+		company = Company(supplier = Supplier(size = 1, location = cards[0]), hand = Hand(cards = new_cards) )
+		payment_cards = company._hand._cards[:2]
 
+		tea_cards = TeaCards(supply = supply, catalog = catalog, companies = [company])
+
+		old_neighbors = tea_cards._catalog.get_neighbors(catalog.find_pcard(old_card))
+
+		new_supply = payment_cards + [old_card]
+
+		tea_cards.trade(company, catalog, supply, old_card, new_card, payment_cards)
+		new_neighbors = tea_cards._catalog.get_neighbors(catalog.find_pcard(new_card))
+
+		self.assertEqual(new_supply, tea_cards._supply._cards)
+		self.assertEqual(tea_cards._companies[0]._hand._cards, [cards[2]])
+
+class Test_TeaCards_operate(unittest.TestCase):
+	
+	def test_operate_basic(self):
+		supply = Supply(cards = cards)
+		catalog = Catalog(positioned_cards = pcards)
+		company = Company(supplier = Supplier(size = 2, location = cards[0]), hand = Hand(cards = []))
+		tea_cards = TeaCards(supply = supply, catalog = catalog, companies = [company])
+
+		tea_cards.operate(company, supply, catalog)
+
+		self.assertEqual(tea_cards._supply._cards, [cards[2]])
+	
+	def test_operate_over(self):
+		supply = Supply(cards = cards)
+		catalog = Catalog(positioned_cards = pcards)
+		company = Company(supplier = Supplier(size = 5, location = cards[0]), hand = Hand(cards = []))
+		tea_cards = TeaCards(supply = supply, catalog = catalog, companies = [company])
+
+		tea_cards.operate(company, supply, catalog)
+		print(supply.draw(3), 'DRAW')
+		self.assertEqual(tea_cards._supply._cards, [])
+		self.assertEqual(tea_cards._companies[0]._supplier._size, 4)
+		self.assertEqual(tea_cards._companies[0]._hand._cards, cards)
+
+	def test_operate_under(self):
+		supply = Supply(cards = cards)
+		additional_card = Card(colors = {BLUE, RED}, number = 1)
+		additional_pcard = PositionedCard(card = additional_card, north = cards[0], south = cards[1], east = cards[2], west = cards[0])
+		new_pcards = pcards + [additional_pcard]
+		catalog = Catalog(positioned_cards = new_pcards)
+		company = Company(supplier = Supplier(size = 2, location = additional_card), hand = Hand(cards = []))
+		tea_cards = TeaCards(supply = supply, catalog = catalog, companies = [company])
+
+		tea_cards.operate(company, supply, catalog)
+
+		self.assertEqual(tea_cards._companies[0]._supplier._size, 0)
 
 
 
